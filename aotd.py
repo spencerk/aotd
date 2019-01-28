67f26e8c515852dbd9dd141ef524823f96e6ad85
Spencer Kiser <spencer.kiser@metmuseum.org>
2019-01-27 20:12:56 -0500

    pulled out fake route

--- aotd.py
+++ aotd.py
@@ -5,52 +5,8 @@ import _json
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/fake')
+@app.route('/today')
 def aotd():
-    term = "English Civil War"
-    #proper1 = ["English Civil War", "Emperor Heraclius", "Emperor Khorau", "Persian", "General Rhahzadh", "Mosul", "Iraq"]
-    #proper2 = ["England", "Oaks Colliery"]
-    #proper3 = ["December Uprising", "Moscow", "Council of Workers' Deputies of Kiev", "Shuliavka Republic"]
-    #proper4 = ["Generalissimo Chiang Kai-shek", "Republic of China", "Marshal Zhang Xueliang", "Manchuria"]
-    #proper5 = ["Clapham Junction", "London"]
-
-    oldpage = requests.get("https://en.wikipedia.org/wiki/Main_Page", verify=False)
-
-    soup = BeautifulSoup(oldpage.text, 'html.parser')
-
-    soupy= (soup.find('div',{"id": "mp-otd"}))
-
-    soupy1= (soupy.find('ul'))
-
-    lis=soupy1.find_all('li')
-    #for elem in lis:
-        #records['history'](elem.text.strip())
-
-    #for i in proper1:
-    record = {}
-    record['history'] = lis[0].text.strip()
-    record['searchterm'] = "English Civil War"
-    record['reqURL'] = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=%s" % record['searchterm']
-    searchReq = requests.get(record['reqURL'])
-         
-    artObjects = searchReq.json()
-    firstObjectID = artObjects["objectIDs"][0]
-
-    objReq = "https://collectionapi.metmuseum.org/public/collection/v1/objects/%s" % firstObjectID
-    firstObjectData = requests.get(objReq)
-
-    firstObjectJSON = firstObjectData.json()
-    record['img'] = firstObjectJSON['primaryImageSmall']
-    record['title'] = firstObjectJSON['title']
-    record['artist'] = firstObjectJSON['artistDisplayName']
-    record['objURL'] = firstObjectJSON['objectURL']
-
-
-    return render_template('aotd.html', record=record) 
-
-
-@app.route('/real')
-def aotd_real():
     subscription_key = '02beee7ce5844779b21fa4733308236a'
     assert subscription_key
 
@@ -70,8 +26,6 @@ def aotd_real():
     headers = {"Ocp-Apim-Subscription-Key": subscription_key}
     response = requests.post(key_phrase_api_url, headers=headers, json=documents)
     key_phrases = response.json()
-
-    #print key_phrases
         
     records = [dict() for x in range(0,len(occurrences))] 
     for i in range(0,len(occurrences)):
@@ -100,7 +54,6 @@ def aotd_real():
                     records[i]['searchTerms'][j]['artist'] = firstObjectJSON['artistDisplayName']
                     records[i]['searchTerms'][j]['objURL'] = firstObjectJSON['objectURL']
 
-    print records
 
     return render_template('aotd.html', records=records) 
 
